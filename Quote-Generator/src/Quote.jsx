06138d32a8a8a16abd67e221547f5e843f0cfe54
hanifubaaba <baabahanif88@gmail.com>
2025-04-12 15:34:08 +0100

    Created quote component

---
+++ Quote-Generator/src/Quote.jsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+
+export default function Quote() {
+  const [quote, setQuote] = useState({
+    text: "Life is a succession of lessons which must be lived to be understood.",
+    author: "Ralph Waldo Emerson",
+  });
+
+  const API_URL = "https://api.api-ninjas.com/v1/quotes";
+  const API_KEY = "GjPcFqohZlHh++lJrN7uZg==9YA7FXQC2TiUhme1";
+  async function fetchQuote() {
+    try {
+      const res = await fetch(API_URL, {
+        headers: {
+          "X-Api-Key": API_KEY,
+        },
+      });
+      const data = await res.json();
+      const quoteObj = data[0];
+      setQuote({
+        text: quoteObj.quote || quoteObj.contentObj,
+        author: quoteObj.author,
+        category: quoteObj.category,
+      });
+    } catch (error) {
+      console.error("Could'nt Fetch Quote, Try again.", error);
+    }
+  }
+  useEffect(() => {
+    fetchQuote();
+  }, []);
+
+  function getQuote() {
+    fetchQuote();
+  }
+
+  return (
+    <div>
+      <div>
+        <h1>{quote.text}</h1>
+        <h3>{quote.author}</h3>
+        <h3>{quote.category}</h3>
+      </div>
+      <button className="button" onClick={getQuote}>
+        {" "}
+        Get Quote
+      </button>
+      <div className="flex justify-center items-center min-h-screen bg-blue-100">
+        <h1 className="text-4xl text-blue-500 font-bold">
+          Tailwind CSS is working!
+        </h1>
+      </div>
+    </div>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+export const content = [
+  "./index.html",
+  "./src/**/*.{js,jsx,ts,tsx}", // Vite will scan these files for Tailwind classes
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
